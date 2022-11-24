90223d9a7a9e7bba042e29341e537757d706c3e2
Liên Thị Quỳnh Nga <ngaltq@bkhightech.vn>
2022-11-24 07:58:40 +0700

    add image

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,29 +1,29 @@
-const staffsRouter = require("./staffs");
+// const staffsRouter = require("./staffs");
 const customersRouter = require("./customers");
 const departmentsRouter = require("./departments");
-const rolesRouter = require("./roles");
+// const rolesRouter = require("./roles");
 const customerTypesRouter = require("./customerTypes");
-const ratesRouter = require("./rates");
-const notificationsRouter = require("./notifications");
+// const ratesRouter = require("./rates");
+// const notificationsRouter = require("./notifications");
 const bookingsRouter = require("./bookings");
-const imagesRouter = require("./images");
-const serviceTypesRouter = require("./serviceTypes");
-const servicesRouter = require("./services");
+// const imagesRouter = require("./images");
+// const serviceTypesRouter = require("./serviceTypes");
+// const servicesRouter = require("./services");
 const billsRouter = require("./bills");
 const advertisementsRouter = require("./advertisements");
 
 function route(app) {
-  app.use("/api/v1/staffs", staffsRouter);
+  // app.use("/api/v1/staffs", staffsRouter);
   app.use("/api/v1/customers", customersRouter);
   app.use("/api/v1/departments", departmentsRouter);
-  app.use("/api/v1/roles", rolesRouter);
+  // app.use("/api/v1/roles", rolesRouter);
   app.use("/api/v1/customerTypes", customerTypesRouter);
-  app.use("/api/v1/rates", ratesRouter);
-  app.use("/api/v1/notifications", notificationsRouter);
+  // app.use("/api/v1/rates", ratesRouter);
+  // app.use("/api/v1/notifications", notificationsRouter);
   app.use("/api/v1/bookings", bookingsRouter);
-  app.use("/src/images", imagesRouter);
-  app.use("/api/v1/serviceTypes", serviceTypesRouter);
-  app.use("/api/v1/services", servicesRouter);
+  // app.use("/src/images", imagesRouter);
+  // app.use("/api/v1/serviceTypes", serviceTypesRouter);
+  // app.use("/api/v1/services", servicesRouter);
   app.use("/api/v1/bills", billsRouter);
   app.use("/api/v1/advertisements", advertisementsRouter);
 }
